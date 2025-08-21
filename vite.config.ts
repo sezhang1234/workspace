08c368a26c604b59c6c29f3ee0c82d1737c312a1
Sean Zhang <touch.zhang@gmail.com>
2025-08-21 11:40:23 +0800

    integrate flowgram to implement workflow canvas

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true }]
+      ]
+    }})],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
